6079212e39725b9f519a4ce0028ccd0ffa2abf1f
rabin-nyaundi <nyaundirabin@gmail.com>
2020-12-06 13:07:38 +0300

    fixing static files error

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -82,19 +82,19 @@ WSGI_APPLICATION = 'ecomerce.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'shop_db',
-        'USER': 'admin2020',
-        'PASSWORD': 'admin@2020',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'shop_db',
+    #     'USER': 'admin2020',
+    #     'PASSWORD': 'admin@2020',
+    #     'HOST': 'localhost',
+    #     'PORT': '',
+    # }
 }
 
 
