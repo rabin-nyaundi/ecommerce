076338ebcd48d981b19f98daadc1d6d1d798e17f
rabin-nyaundi <nyaundirabin@gmail.com>
2020-12-06 12:36:34 +0300

    Added whitenoise to requirements.txt

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -137,9 +137,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 MEDIA_URL = '/media/'
 
